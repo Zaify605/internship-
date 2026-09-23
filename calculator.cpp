58c27fce61400acd2ec04cebe795808a7795910c
Zaify605 <huzaifasarfraz473@gmail.com>
2026-09-23 15:12:19 +0500

    Add calculator

---
+++ calculator.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int x,y;
+	cout<<"enter two values=";
+	cin>>x;
+	cin>>y;
+	
+	cout<<"product is="<<x*y<<endl;
+	cout<<"division is="<<x/y<<endl;
+	cout<<"addition is="<<x+y<<endl;
+	cout<<"sutraction is="<<x-y<<endl;
+}
